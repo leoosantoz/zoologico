29a8defc2fd8ff41289b7e3a4a0477dab2dafdf3
leoosantoz <leosilvaviper@gmail.com>
2024-09-06 21:35:15 -0300

    tarefa julio zoologico

--- br.com.zoologico.animal/src/br/com/zoologico/animal/mamifero.java
+++ br.com.zoologico.animal/src/br/com/zoologico/animal/mamifero.java
@@ -2,23 +2,23 @@ package br.com.zoologico.animal;
 
 public class mamifero extends animal {
 
-	private String pelo;
-	
-	public mamifero(String nome, int idade, String pelo) {
-		super(nome, idade);
-		this.pelo = pelo;
-	}
-	
-	public void amamentar() {
-		System.out.println("Amamentando. .");
-	}
-	
-	@Override
-	public void exibirInfo() {
-		// TODO Auto-generated method stub
-		super.exibirInfo();
-		System.out.println ("Pelo: "+pelo);
-	}
+    private String pelo;
 
-	
+    public mamifero(String nome, int idade, String pelo) {
+        super(nome, idade);
+        this.pelo = pelo;
+    }
+
+    public void amamentar() {
+        System.out.println("Amamentando. .");
+    }
+
+    @Override
+    public void exibirInfo() {
+        // TODO Auto-generated method stub
+        super.exibirInfo();
+        System.out.println("Pelo: " + pelo);
+    }
+
+
 }
